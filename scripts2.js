b326d8db9e822932a521b80fc923a0514a285b24
juanRodriguez <juanmrodriguez1989@gmail.com>
2021-05-23 18:02:58 -0300

    mod 22/05/21

--- scripts2.js
+++ scripts2.js
@@ -2,6 +2,7 @@
 
 const canvas = document.querySelector("#canvas1");
 const ctx = canvas.getContext("2d");
+
 const CANVAS_WIDTH = (canvas.width = 1000);
 const CANVAS_HEIGHT = (canvas.height = 800);
 
@@ -27,7 +28,13 @@ const staggerFrames = 18;
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.font = "14px Heebo";
 
+  ctx.fillText(
+    "Raiden spritesheet - " + monkuroWidth + " x " + monkuroWidth,
+    108,
+    125
+  );
   //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
   ctx.drawImage(
     monkuro,
@@ -53,3 +60,20 @@ function animate() {
 }
 
 animate();
+
+/* mandar los characters a estas clases 
+
+// clase de pjs
+
+class Character {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+const monkuro = new Character(104, 135);
+
+const raiden = new Character(108, 125);
+
+*/
